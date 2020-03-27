f53aa0963d218563df733faf63619394a891bb32
Kale Kundert <kale@thekunderts.net>
2020-03-27 12:39:01 -0400

    Implement basic functionality.

---
+++ po4/cli/main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""\
+Query the DNA database.
+
+Usage:
+    po4 databases
+    po4 check
+
+Commands:
+    databases
+        List available databases.
+
+    check
+        Make sure everything in the current database makes sense.
+"""
+
+def main():
+    print("Command-line tool not yet implemented.")
+
