5980b18abf89f70b94ed72f1ced9619f9d4d9683
Kale Kundert <kale@thekunderts.net>
2020-05-07 11:23:16 -0400

    Add the `query` subcommand.

--- po4/cli/main.py
+++ po4/cli/main.py
@@ -4,14 +4,23 @@
 Query the DNA database.
 
 Usage:
+    po4 query <tags>... [--attrs <list>]
     po4 check [<db>]
     po4 which
 
 Commands:
+    query:  Display information about of the given constructs.
     check:  Make sure everything in the current database makes sense.
     which:  List the available databases.
 
+Options:
+    -a --attrs <list>      [default: seq]
+        A comma-separated list of attributes to display.
+
 Arguments:
+    <tags>
+        One or more tags to search for in the database.
+
     <db>
         The name of a database to use.  This corresponds to the "use" setting 
         in the configuration files.  If not given, the default database for the 
@@ -20,16 +29,40 @@ Arguments:
 
 import docopt
 from tqdm import tqdm
-from .. import load_config, load_db, CheckError
+from po4 import load_config, load_db, CheckError, QueryError
 
 def main():
     args = docopt.docopt(__doc__)
+
+    if args['query']:
+        seq(args['<tags>'], args['--attrs'].split(','))
 
     if args['check']:
         check(args['<db>'])
 
     if args['which']:
         which()
+
+def seq(tags, attrs):
+    db = load_db()
+
+    print('\t'.join(['tag', *attrs]))
+
+    for tag in tags:
+        try:
+            hit = db[tag]
+        except QueryError:
+            print(tag)
+            continue
+
+        row = []
+        for attr in attrs:
+            try:
+                row.append(getattr(hit, attr))
+            except (QueryError, AttributeError):
+                row.append('')
+
+        print('\t'.join(str(x) for x in [hit.tag, *row]))
 
 def check(use=None):
     db = load_db(use)
