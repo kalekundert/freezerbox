005fb8b161a1c510b9a5ed5b32fc8657190dbab6
Kale Kundert <kale@thekunderts.net>
2021-05-02 09:53:06 -0400

    refactor: move `@only_raise` into tidyexc

--- freezerbox/errors.py
+++ freezerbox/errors.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import functools
-from tidyexc import Error
+from tidyexc import Error, only_raise
 
 class Po4Error(Error):
 
@@ -34,28 +34,3 @@ class CheckError(Po4Error):
     pass
 
 
-
-class only_raise:
-    """
-    Guarantee that the decorated function can only raise the given type of 
-    exception.
-
-    Any unhandled exception raised by the decorated function will be caught and 
-    re-raised using an exception of the given type. 
-    """
-
-    def __init__(self, err_cls):
-        self.err_cls = err_cls
-
-    def __call__(self, f):
-
-        @functools.wraps(f)
-        def wrapper(*args, **kwargs):
-            try:
-                return f(*args, **kwargs)
-            except self.err_cls as err:
-                raise err from None
-            except Exception as err:
-                raise self.err_cls(str(err)) from err
-
-        return wrapper
